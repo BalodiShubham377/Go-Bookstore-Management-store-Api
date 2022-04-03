fb5991f61d0fbbe027be668d1eff96656efe6521
unknown <shubhambalodi79@gmail.com>
2022-04-03 16:20:01 +0530

    updated

---
+++ cmd/main/main.go
@@ -0,0 +1,16 @@
+package main
+
+import(
+	"log"
+	"net/http"
+	"github.com/gorilla/mux"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/BalodiShubham377/Go-Bookstore-Management-store-Api/pkg/routes"
+)
+
+func main(){
+	r := mux.NewRouter()
+	routes.RegisterBookStoreRoutes(r)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe("localhost:9010", r))
+}
